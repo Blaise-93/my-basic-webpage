950953c5f11babc302369d008eb525e3680797fb
Blaise <ejikemeblaise08@gmail.cim>
2022-11-01 23:10:59 +0100

    Added boostrap

--- src/components/LinkSection.js
+++ src/components/LinkSection.js
@@ -10,45 +10,86 @@ class LinkSection extends Component {
         pitch_id: 'https://background.zuri.team',
         design: "https://books.zuri.team/design-rules"      
     }
-
     btn_id = {
-        margin: 16,
-        padding: 40,
-        border: 0,
-        borderRadius: 30,
-        outline: 'none',
-        backgroundColor: '#FFF',
-        fontSize: 15,
-        cursor: 'pointer',
-        fontFamily: 'sansSerif',
-        fontWeight: 700
+        backgroundColor: '4CAF50',
+        border: 'none',
+        color: 'white',
+        padding: '15px 32px',
+        textAlign: 'center',
+        textDecoration: 'none',
+        display: 'inline-block',
+        fontSize: 16,
       }
-  
-    
    
   
+      btn_books_id = {
+        backgroundColor: '4CAF50',
+        border: 'none',
+        color: 'white',
+        padding: '15px 32px',
+        textAlign: 'center',
+        textDecoration: 'none',
+        display: 'inline-block',
+        fontSize: 16,
+      }
+   
+      btn_books_python = {
+        backgroundColor: '4CAF50',
+        border: 'none',
+        color: 'white',
+        padding: '15px 32px',
+        textAlign: 'center',
+        textDecoration: 'none',
+        display: 'inline-block',
+        fontSize: 16,
+      }
+   
+  
+      btn_pitch_id = {
+        backgroundColor: '4CAF50',
+        border: 'none',
+        color: 'white',
+        padding: '15px 32px',
+        textAlign: 'center',
+        textDecoration: 'none',
+        display: 'inline-block',
+        fontSize: 16,
+      }
+
+      btn_design_id = {
+        backgroundColor: '4CAF50',
+        border: 'none',
+        color: 'white',
+        padding: '15px 32px',
+        textAlign: 'center',
+        textDecoration: 'none',
+        display: 'inline-block',
+        fontSize: 16,
+      }
+    
+
 
     render() {
         return (
-       <div >
+       <div className='p-3'>
             <h3 className='mt-3 text-info'>
                 Welcome to our book marketplace. 
             </h3>
             <p>Your home of affordable products is here.</p>
-            <button a style={this.btn_id}><a href={this.state.btn_anchor_id} >Zuri</a></button>
+            <button style={this.btn_id}><a href={this.state.btn_anchor_id} >Zuri</a></button>
             <hr/>
             <p>This is where you find books about design and coding.Kindly checkout by clicking the button.</p>
-            <button ><a href={this.state.books} >Zuri Books</a></button>
+            <button style={this.btn_books_id}><a href={this.state.books} >Zuri Books</a></button>
             <hr/>
             <p>This where you feature the book as if you were selling it.Kindly checkout by clicking the button.</p>
             <p>NB: Your reference id - {this.state.ref_id} must be unique and will give you royalties if any sales of the book come.</p>
-            <button ><a href={this.state.books_python} >Zuri Searches</a></button>
+            <button style={this.btn_books_python}><a href={this.state.books_python} >Zuri Searches</a></button>
             <hr/>
             <p>Hi there, here where where you pitch a service for doing background checks on coders. Our products are mouthwatering and unique. Kindly check it out</p>
-            <button ><a href={this.state.pitch_id} >Zuri Background Check</a></button>
+            <button style={this.btn_pitch_id}><a href={this.state.pitch_id} >Zuri Background Check</a></button>
             <hr/>
             <p>Here, you  will pitch the free design book offered by Zuri. Our products are mouthwatering and unique. Kindly check it out via clicking the button.</p>
-            <button ><a href={this.state.design} >Product Design</a></button>
+            <button style={this.btn_design_id}><a href={this.state.design} >Product Design</a></button>
 
         </div>
         );
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -8,7 +8,10 @@ class Profile extends Component {
         profile_imageUrl: 'https://media-exp1.licdn.com/dms/image/C4D03AQHAPU730L-q5w/profile-displayphoto-shrink_800_800/0/1650720157580?e=2147483647&v=beta&t=gbfNwLuJY6uYJ3UJrLsd_HE2FPwMuf71IDX9F4IVQIU',
         
     }
-    
+    slack_hidden = {
+        textIndent: -9999, 
+        whiteSpace: 'nowrap'
+    }
    
 
     render() {
@@ -20,12 +23,12 @@ class Profile extends Component {
             <p>My profile details are:
                 <ul>
                     <li><span>Twitter id - </span>{this.state.twitter_id}</li>
-                    <li> <span>slack_id - </span>{this.state.slack_username}</li>
+                    <li styles={this.slack_hidden }> <span >slack_id - </span>{this.state.slack_username}</li>
                 </ul>
                </p>
              
-               <img src={this.state.profile_imageUrl} alt='Blaisemart'/>
-               <figcaption>Blaise Profile Picture</figcaption>
+               <img className='mx-auto d-block img-fluid' src={this.state.profile_imageUrl} alt='Blaisemart'/>
+               <figcaption className='text-center'>Blaise Profile Picture</figcaption>
            
         </div>
         );
